f82846212b0948e131ec03c08419519dc3d92796
AndMe <andrevieira@lme.ufc.br>
2022-01-12 19:25:22 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Button from "./Button"
+import "./style.css"
+
+function sum(a, b) {
+    alert (a + b)
+}
+
+function primeiroJSX() {
+    return (
+        <div>
+            Holivane Holanda - Introdução ao ReactJS
+            <h3>Soma: {sum(10, 20)}</h3>
+        </div>
+    )
+}
+
+function App () {
+    return (
+        <div className="App">
+            Hello World
+            {primeiroJSX};
+            <Button onClick={() => sum(10, 20)} name="Holivane Holanda" />
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
